765124863f11ff34fb3b485081ba08fadd714cda
bencky <58736880+bencky1017@users.noreply.github.com>
2021-11-19 22:19:45 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function(){
+	$('.background').css({
+		'position': 'fixed',
+		'width': '100%',
+		'height': '100%',
+		'background': 'url(https://cdn.seovx.com/?mom=302)',
+		'background-size': 'cover',
+		'background-repeat': 'no-repeat',
+		'background-position': 'center center',
+		'z-index': '-1',
+	});
+});
