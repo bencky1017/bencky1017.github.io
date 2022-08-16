1f56ee9f57d68cf64190c6526d2ae05cd17c9459
bencky <58736880+bencky1017@users.noreply.github.com>
2022-08-16 21:32:31 +0800

    update webwork such as 作品展示

--- about/js/index.js
+++ about/js/index.js
@@ -1,5 +1,5 @@
 $(function(){
-
+	// 作品时间戳
 	var len_ts=ts.worklist.length;
 	for (var i = 0; i < len_ts; i++) {
 		var len_list=ts.worklist[i].list.length;
@@ -20,7 +20,7 @@ $(function(){
 			var list_version=ts.worklist[i].list[j].version==""?'&nbsp;':ts.worklist[i].list[j].version;
 			var list_description=ts.worklist[i].list[j].description;
 			var list_highlight=ts.worklist[i].list[j].highlight;
-			console.log(list_date,list_title,list_version,list_description,list_highlight);
+			// console.log(list_date,list_title,list_version,list_description,list_highlight);
 
 			var worklist=`
 				<li class="cls">
@@ -47,10 +47,39 @@ $(function(){
 		$(this).parents(".worklist").toggleClass("close");
 	});
 
-	for (var i = 1; i <= $('.nav a').length; i++) {
-		if ($('.nav a:nth-child('+i+')').has('i')) {
-			$('.nav a').eq(i).find('i:after').css('content',$('.nav a').eq(3).find('i').data('tip'));
-			console.log(666);
-		}
+	// for (var i = 1; i <= $('.nav a').length; i++) {
+	// 	if ($('.nav a:nth-child('+i+')').has('i')) {
+	// 		$('.nav a').eq(i).find('i:after').css('content',$('.nav a').eq(3).find('i').data('tip'));
+	// 	}
+	// }
+});
+$(function(){
+	// 作品展示
+	var len_webwork=wk.webworklist.length;
+	for (var i = 0; i < len_webwork; i++) {
+		var item_src=wk.webworklist[i].src;
+		var item_title=wk.webworklist[i].item_title;
+		var item_mask=wk.webworklist[i].item_mask;
+		var list=`
+			<div class="webworklist">
+				<img src="`+item_src+`" alt="">
+				<div class="list-item">
+					<div>
+						<p class="item-title">`+item_title+`</p>
+						<p class="item-mask">`+item_mask+`</p>
+					</div>
+				</div>
+			</div>`;
+		$('#work .row').append(list);
+		console.log(666);
 	}
+
+	// 点击赋值
+	$('#work .webworklist').on('click',function(){
+		var getnum=$(this).index();
+		var getlink=wk.webworklist[getnum].link;
+		if (getlink!="") {
+			window.open(getlink);
+		}
+	})
 });
